1b2949f1975a4975318cf4a9f3e15823089337cb
Xiao Han <han.xiao@aalto.fi>
2017-02-20 16:33:34 +0000

    generalized jaccard similarity

--- utils.py
+++ utils.py
@@ -1,5 +1,23 @@
 import numpy as np
-import networkx as nx
-import random
-from copy import copy
+from collections import Counter
 
+
+def generalized_jaccard_similarity(a, b):
+    """a and b should be list
+    >>> a = [1, 1, 2]
+    >>> b = [1, 2, 3]
+    >>> generalized_jaccard_similarity(a, b)
+    0.5
+    >>> generalized_jaccard_similarity(a, a)
+    1.0
+    """
+    a, b = map(Counter, [a, b])
+    all_elements = set(a.keys()) | set(b.keys())
+    counts = [(a[e], b[e]) for e in all_elements]
+    numer = np.sum(np.min(counts, axis=1))
+    denom = np.sum(np.max(counts, axis=1))
+    return numer / denom
+
+
+def generalized_jaccard_distance(a, b):
+    return 1 - generalized_jaccard_similarity(a, b)
