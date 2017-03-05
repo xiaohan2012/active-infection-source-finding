e410b38c6352d4aeffcc4d8c59b13dd2e2554e61
Xiao Han <han.xiao@aalto.fi>
2017-03-05 21:39:16 +0000

    faster cascade generation (4x faster)

--- cascade.py
+++ cascade.py
@@ -1,7 +1,9 @@
+import networkx as nx
 import random
+import numpy as np
 
 
-def generate_cascade(g):
+def generate_cascade_old(g):
     """shit: just use Dijkstra!!
     """
     g = g.copy()
@@ -27,3 +29,27 @@ def generate_cascade(g):
         if not can_continue:
             break
     return infected_times
+
+
+
+def generate_cascade(g):
+    source = random.choice(g.nodes())
+
+    rands = np.random.rand(g.number_of_edges())
+    active_edges = [(u, v) for (u, v), r in zip(g.edges_iter(), rands) if g[u][v]['p'] >= r]
+    induced_g = nx.Graph()
+    induced_g.add_edges_from(active_edges)
+    for u, v in induced_g.edges_iter():
+        induced_g[u][v]['d'] = g[u][v]['d']
+        
+    if not induced_g.has_node(source):
+        infection_times = {n: float('inf') for n in g.nodes_iter()}
+        infection_times[source] = 0
+    else:
+        infection_times = nx.shortest_path_length(induced_g, source=source, weight='d')
+        for n in g.nodes_iter():
+            if n not in infection_times:
+                infection_times[n] = float('inf')
+    assert infection_times[source] == 0
+    assert len(infection_times) == g.number_of_nodes()
+    return infection_times
