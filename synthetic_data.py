c4f93b7a283af20a9c2cc465ce1feff3cdb073be
Xiao Han <han.xiao@aalto.fi>
2017-03-10 10:46:12 +0000

    network with different sizes via cmd

--- synthetic_data.py
+++ synthetic_data.py
@@ -1,3 +1,4 @@
+import argparse
 import pickle as pkl
 import networkx as nx
 from networkx.generators.random_graphs import random_powerlaw_tree
@@ -5,14 +6,22 @@ from graph_generator import kronecker_random_graph, grid_2d, \
     P_peri ,P_hier, P_rand, add_p_and_delta
 from ic import infection_time_estimation
 
-KRONECKER_RAND = 'kronecker-rand'
-KRONECKER_PERI = 'kronecker-peri'
-KRONECKER_HIER = 'kronecker-hier'
+KRONECKER_RAND = 'kr-rand'
+KRONECKER_PERI = 'kr-peri'
+KRONECKER_HIER = 'kr-hier'
 GRID = 'grid'
-PL_TREE = 'powerlaw_tree'
-ER = 'erdos-renyi'
+PL_TREE = 'pl-tree'
+ER = 'er'
 BARABASI = 'barabasi'
 CLIQUE = 'clique'
+all_graph_types = [KRONECKER_RAND,
+                   KRONECKER_PERI,
+                   KRONECKER_HIER,
+                   GRID,
+                   PL_TREE,
+                   ER,
+                   BARABASI,
+                   CLIQUE]
 
 
 INF_TIME_PROBA_FILE = 'inf_time_proba_matrix'
@@ -44,48 +53,66 @@ if __name__ == "__main__":
     
     p = 0.7
     delta = 1
-    
-    gtype = BARABASI
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--type', choices=all_graph_types,
+                        help='graph type')
+    parser.add_argument('-s', '--size', type=int,
+                        default=0,
+                        help="size of graph")
+    parser.add_argument('-e', '--size_exponent', type=int,
+                        default=1,
+                        help="exponent of the size")
+    parser.add_argument('-b', '--exponent_base', type=int,
+                        default=10,
+                        help="base of the size exponent")
 
+    args = parser.parse_args()
+    gtype = args.type
+    if args.size:
+        size = args.size
+        output_dir = 'data/{}/{}'.format(gtype, size)
+    else:
+        size = args.exponent_base ** args.size_exponent
+        output_dir = 'data/{}/{}-{}'.format(gtype, args.exponent_base,
+                                            args.size_exponent)
     if gtype == KRONECKER_HIER:
-        g = gen_kronecker(P=P_hier, k=10, n_edges=2048)
+        g = gen_kronecker(P=P_hier, k=args.size_exponent, n_edges=2**args.size_exponent * 3)
     elif gtype == KRONECKER_PERI:
-        g = gen_kronecker(P=P_peri, k=10, n_edges=2048)
+        g = gen_kronecker(P=P_peri, k=args.size_exponent, n_edges=2**args.size_exponent * 3)
     elif gtype == KRONECKER_RAND:
-        g = gen_kronecker(P=P_rand, k=10, n_edges=2048)
+        g = gen_kronecker(P=P_rand, k=args.size_exponent, n_edges=2**args.size_exponent * 3)
     elif gtype == PL_TREE:
         p = 0.88
-        g = random_powerlaw_tree(100, tries=10000)
+        g = random_powerlaw_tree(size, tries=10000)
     elif gtype == ER:
-        g = extract_larges_CC(nx.fast_gnp_random_graph(100, 0.2))
+        g = extract_larges_CC(nx.fast_gnp_random_graph(size, 0.2))
     elif gtype == BARABASI:
-        g = extract_larges_CC(nx.barabasi_albert_graph(100, 5))
+        g = extract_larges_CC(nx.barabasi_albert_graph(size, 5))
     elif gtype == GRID:
-        g = grid_2d(10)
+        g = grid_2d(size)
     elif gtype == CLIQUE:
-        g = nx.complete_graph(10)
+        g = nx.complete_graph(size)
     else:
         raise ValueError('unsupported graph type {}'.format(gtype))
 
     g.remove_edges_from(g.selfloop_edges())
     print('|V|={}, |E|={}'.format(g.number_of_nodes(), g.number_of_edges()))
 
-    directory = 'data/{}'.format(gtype)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     print('graph type: {}'.format(gtype))
     g = add_p_and_delta(g, p, delta)
-    nx.write_gpickle(g, 'data/{}/graph.gpkl'.format(gtype))
+    nx.write_gpickle(g, '{}/graph.gpkl'.format(output_dir, gtype))
 
     n_rounds = 100
     time_probas, node2id = infection_time_estimation(g, n_rounds,
                                                      return_node2id=True)
     pkl.dump(time_probas,
-             open('data/{}/{}.pkl'.format(gtype, INF_TIME_PROBA_FILE), 'wb'))
+             open('{}/{}.pkl'.format(output_dir, INF_TIME_PROBA_FILE), 'wb'))
 
     pkl.dump(node2id,
-             open('data/{}/{}.pkl'.format(gtype, NODE2ID_FILE), 'wb'))
+             open('{}/{}.pkl'.format(output_dir, NODE2ID_FILE), 'wb'))
     id2node = {i: n for n, i in node2id.items()}
     pkl.dump(id2node,
-             open('data/{}/{}.pkl'.format(gtype, ID2NODE_FILE), 'wb'))
+             open('{}/{}.pkl'.format(output_dir, ID2NODE_FILE), 'wb'))
