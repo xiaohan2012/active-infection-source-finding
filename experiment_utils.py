e75f8f547f026c812c8cca8c110ed8e0960e4260
Xiao Han <han.xiao@aalto.fi>
2017-03-13 14:28:22 +0000

    indentation fixed

--- experiment_utils.py
+++ experiment_utils.py
@@ -6,20 +6,21 @@ from tqdm import tqdm
 from baselines import baseline_dog_tracker
 
 
-def experiment_mwu_multiple_rounds(rounds, g,
-								   node2id, id2node,
-								   s2n_probas,
-								   fraction, epsilon, sampling_method,
-								   query_selection_method,
-								   check_neighbor_threshold,
-								   max_iter=float('inf'),
-								   seed=None):
+def experiment_mwu_multiple_rounds(rounds,
+                                   g,
+                                   node2id, id2node,
+                                   s2n_probas,
+                                   fraction, epsilon, sampling_method,
+                                   query_selection_method,
+                                   check_neighbor_threshold,
+                                   max_iter=float('inf'),
+                                   seed=None):
     np.random.seed(seed)
     random.seed(seed)
     results = []
     for i in tqdm(range(rounds)):
         source, obs_nodes, infection_times, tree = make_partial_cascade(
-			g, fraction, sampling_method=sampling_method)
+            g, fraction, sampling_method=sampling_method)
         r = mwu(g, node2id, id2node,
                 source, obs_nodes, infection_times,
                 s2n_probas,
@@ -31,11 +32,12 @@ def experiment_mwu_multiple_rounds(rounds, g,
         results.append(r)
     return results
 
+
 def experiment_dog_multiple_rounds(rounds, g, fraction, sampling_method):
-	cnts = []
-	for i in range(rounds):
-		source, obs_nodes, infection_times, tree = make_partial_cascade(
-			g, fraction, sampling_method=sampling_method)
-		c = baseline_dog_tracker(g, obs_nodes, infection_times)
-		cnts.append(c)
-	return cnts
+    cnts = []
+    for i in range(rounds):
+        source, obs_nodes, infection_times, tree = make_partial_cascade(
+            g, fraction, sampling_method=sampling_method)
+        c = baseline_dog_tracker(g, obs_nodes, infection_times)
+        cnts.append(c)
+    return cnts
