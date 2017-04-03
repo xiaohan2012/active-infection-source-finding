8a8b3988f7dcf694727794920f709633eb55897f
Xiao Han <han.xiao@aalto.fi>
2017-04-03 13:01:04 +0000

    re-aranged order

--- scripts/source_likelihood_vs_p_and_q.sh
+++ scripts/source_likelihood_vs_p_and_q.sh
@@ -3,8 +3,8 @@
 gtypes=(kr-rand kr-peri kr-hier balanced-tree er barabasi)
 exps=(6 7 8 9)
 
-for gtype in $gtypes; do
-    for exp in $exps; do
+for exp in $exps; do
+    for gtype in $gtypes; do    
 	eval "python how-well-can-we-model-probability.py $gtype 2-$exp"
     done
 done
