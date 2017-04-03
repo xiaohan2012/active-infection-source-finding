3ad059599e0d455e5f01973b56c64bb3c6161cd1
Xiao Han <han.xiao@aalto.fi>
2017-04-03 11:43:35 +0000

    using small kronecker graph to control the graph size for source likelihood experiment"

--- scripts/source_likelihood_vs_p_and_q.sh
+++ scripts/source_likelihood_vs_p_and_q.sh
@@ -1,6 +1,12 @@
 #! /bin/zsh
 
-params=("pl-tree 2-6" "balanced-tree 2-6" "barabasi 2-6" "kr-peri 10-8" "kr-hier 10-8" "kr-rand 10-8" "p2p-gnutella08 ''")
+params=("pl-tree 2-6"
+	"balanced-tree 2-6"
+	"er 2-6"
+	"barabasi 2-6"
+	"kr-peri 10-6"
+	"kr-hier 10-6"
+	"kr-rand 10-6")
 
 for param in $params; do
     eval "python how-well-can-we-model-probability.py $param"
