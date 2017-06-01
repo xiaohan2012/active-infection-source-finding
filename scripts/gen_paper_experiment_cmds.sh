d2d4590cbe171ab04f601658fbd383d211c0205a
Han Xiao <han.xiao@aalto.fi>
2017-06-01 19:59:38 +0000

    tbfs cmds

--- scripts/gen_paper_experiment_cmds.sh
+++ scripts/gen_paper_experiment_cmds.sh
@@ -2,7 +2,7 @@
 
 # gtypes=(p2p-gnutella08 arxiv-hep-th enron-email dblp-collab)
 gtypes=(p2p-gnutella08 arxiv-hep-th enron-email)
-methods=(truncated_mst)
+methods=(tbfs)
 model_params=("si -p 0.1" "ic -p 0.5" "sp -p 0.1")
 qs=(0.005 0.01 0.015 0.02 0.025 0.03 0.035 0.04 0.045 0.05 0.055 0.06 0.065 0.07 0.075 0.08 0.085 0.09 0.095 0.1)
 
