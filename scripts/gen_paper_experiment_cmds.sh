a9d3325638cc20398a6c21c7f9247a7aac698a5b
Han Xiao <han.xiao@aalto.fi>
2017-06-07 08:35:30 +0000

    rerun ic on p2p using epidemic threshold

--- scripts/gen_paper_experiment_cmds.sh
+++ scripts/gen_paper_experiment_cmds.sh
@@ -2,30 +2,30 @@
 
 # gtypes=(p2p-gnutella08 arxiv-hep-th enron-email dblp-collab)
 # gtypes=(p2p-gnutella08 arxiv-hep-th enron-email)
-# gtypes=(p2p-gnutella08)
+gtypes=(p2p-gnutella08)
 # gtypes=(arxiv-hep-th enron-email)
 # gtypes=("barabasi/2-6/" "barabasi/2-7/" "barabasi/2-8/" "barabasi/2-9/"
 # 	"barabasi/2-10/" "barabasi/2-11/" "barabasi/2-12/" "barabasi/2-13/"
 # 	"barabasi/2-14/")
-gtypes=("barabasi/2-12")
+# gtypes=("barabasi-64")
 # gtypes=("grid-64")
 # gtypes=("grid-64")
 # methods=(mst tbfs no-order greedy)
 # methods=(mst tbfs no-order)
-methods=(tbfs)
-# model_params=("si -p 0.1" "ic -p 0.5" "sp -p 0.1")
-# model_params=("si -p 0.1" "ic -p 0.083" "sp -p 0.1")
-# model_params=("ic -p 0.083")  #
-# model_params=("ic -p 0.2660444431189779")  #  grid
-# p2p: 0.035241715776066926
-# model_params=("ic -p 0.035241715776066926")
-# arxiv: 0.03222184517702736
-# enron: 0.00844468246106171
-model_params=("si -p 0.1")
+methods=(mst tbfs no-order)
+# model_params=("si -p 0.1")
 
-# qs=(0.005 0.01 0.015 0.02 0.025 0.03 0.035 0.04 0.045 0.05 0.055 0.06 0.065 0.07 0.075 0.08 0.085 0.09 0.095 0.1)
+# model_params=("ic -p 0.0831108693813754")  # barabasi
+# model_params=("ic -p 0.2660444431189779")  #  grid
+model_params=("ic -p 0.035241715776066926")  #  p2p
+# model_params=("ic -p 0.03222184517702736")  #  arxiv
+# model_params=("ic -p 0.00844468246106171")  #  enron
+
+# model_params=("si -p 0.1")
+
+qs=(0.005 0.01 0.015 0.02 0.025 0.03 0.035 0.04 0.045 0.05 0.055 0.06 0.065 0.07 0.075 0.08 0.085 0.09 0.095 0.1)
 # qs=(0.1)
-qs=(0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0)
+# qs=(0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0)
 
 for gtype in $gtypes; do
     for method in $methods; do
@@ -41,7 +41,7 @@ for gtype in $gtypes; do
 		if [[ ! -a ${check_path} ]]; then
 		    print "python paper_experiment.py -g ${gtype}  -m ${method}  -l ${model_param}  -q ${q}  -o outputs/paper_experiment/${gtype}/${model}/${method}/qs/${q}.time.pkl -k 100"
 		else
-		    # print "python paper_experiment.py -g ${gtype}  -m ${method}  -l ${model_param}  -q ${q}  -o outputs/paper_experiment/${gtype}/${model}/${method}/qs/${q}.pkl -k 100"
+		    print "python paper_experiment.py -g ${gtype}  -m ${method}  -l ${model_param}  -q ${q}  -o outputs/paper_experiment/${gtype}/${model}/${method}/qs/${q}.pkl -k 100"
 		    # print done
 		fi
 		
